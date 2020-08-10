026e941d5b2239d69575e36052c5d838b0edfc11
W0ulf <wout.depit@gmail.com>
2020-08-10 19:17:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,42 @@ bot.on('ready', () =>{
 });
 
 
+
 bot.on('message', message=>{
     
     let args = message.content.substring(PREFIX.length).split(" ");
 
     switch(args[0]){
+        case 'warn':
+            module.exports.run = async (client,message, args) => {
+            if (!message.member.hasPermission("BAN_MEMBER")) return message.reply("You don\'t have permissions to use this command!");
+            if (!args[1]) return message.reply('You did not specify a user.');
+            if (!args[2]) return message.reply('You did not give a reasoning.')
+            if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.reply("I don't have perms to warn this person");
+            if (!warnUser) return message.reply("Couldn't find that user!");
+            var warnUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
+            var reason = args.slice(2).join("");
+            if (warnUser.hasPermission("MANAGE_MESSAGE")) return message.reply("You can\'t warn this user!");
+            if (!warns[warnUser.id]) warns[warnUser.id] ={
+                warns: 0
+            };
+            warns[warnUser.id].warns++;
+            fs.writeFile("./warnings.json", JSON.stringify(warns), (err) =>{
+                if (err) console.log(err);
+            });
+            var embed = new Discord.MessageEmbed()
+                .setColor("0x17CFEC")
+                .setFooter(message.member.displayName, message.author.displayAvatarURL)
+                .setTitle('**Warned** ${warnUser} (${warnUser.id})')
+                .setDescription('**Reason:** ${reason}')
+                .addField("Amount of warns", warns[warnUser.id].warns);
+            var channel = message.member.guild.channels.chache.get("742420391057948783");
+            if(!channel) return;
+            channel.send(embed);
+            }
+    break;
         case 'warnrules':
+
             if(message.member.permissions.has('ADMINISTRATOR')){
                 const Embed = new Discord.MessageEmbed()
             .setTitle("These will result in a warning/verbal")
@@ -189,32 +219,6 @@ bot.on('message', message=>{
             .addField("-ranks", "Shows The Black Hand ranks embed.")
              .setColor(0x6FC618)
              message.channel.send(Embed);
-        break;
-        case 'warn':
-                if (!args[1]) return message.reply('You did not specify a user.');
-                if (!args[2]) return message.reply('You did not give a reasoning.')
-                if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.reply("I don't have perms to warn this person");
-                
-                var warnUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
-                var reason = args.slice(2).join("");
-                if (warnUser.hasPermission("MANAGE_MESSAGE")) return message.reply("You can\'t warn this user!");
-                if (!warns[warnUser.id]) warns[warnUser.id] ={
-                    warns: 0
-                };
-                warns[warnUser.id].warns++;
-                fs.writeFile("./warnings.json", JSON.stringify(warns), (err) =>{
-                    if (err) console.log(err);
-                });
-                var embed = new Discord.MessageEmbed()
-                    .setColor("0x17CFEC")
-                    .setFooter(message.member.displayName, message.author.displayAvatarURL)
-                    .setTitle('**Warned** ${warnUser} (${warnUser.id})')
-                    .setDescription('**Reason:** ${reason}')
-                    .addField("Amount of warns", warns[warnUser.id].warns);
-                var channel = message.member.guild.channels.chache.get("742420391057948783");
-                if(!channel) return;
-                channel.send(embed);
-
         break;
         case 'mute':
             if(message.member.permissions.has('MANAGE_MESSAGE')){
