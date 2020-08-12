807a916afb0da7c97c12b4615e1fdae5dc52a6cf
W0ulf <wout.depit@gmail.com>
2020-08-12 11:21:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith(
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
 
-    client.commands.set(command.name, commad);
+    bot.commands.set(command.name, command);
 }
 
 
