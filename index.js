588e3c2d9feba5f8c50a07b32e5147b1c40623f6
W0ulf <wout.depit@gmail.com>
2020-08-13 14:59:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith(
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
 
-    client.commands.set(command.name, command);
+    bot.commands.set(command.name, command);
 }
 
 var version = '1.0.4'
