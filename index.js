596e5b305ddad54dc0912caa0f4232502da1b3e5
W0ulf <wout.depit@gmail.com>
2020-08-13 14:14:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const Discord = require('discord.js');
 const ms = require('ms');
-const fs = require("fs");
 const PREFIX = '-';
 const bot = new Discord.Client();
+const Discord = require('discord.js');
+const fs = require("fs");
+
+client.commands =new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, commad);
+}
 
 var version = '1.0.4'
 
