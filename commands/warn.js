83b6cc977b01d653dded82a6daf91ad4064b847e
W0ulf <wout.depit@gmail.com>
2020-08-13 15:42:39 +0200

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,34 +1,33 @@
 const discord = require("discord.js");
 const fs = require("fs");
 const warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"))
+
+module.exports.run = async (client, message, args) => {
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply("You don\'t have permissions to use this command!");
+    if (!args[1]) return message.reply('You did not specify a user.');
+    if (!args[2]) return message.reply('You did not give a reasoning.')
+    if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.reply("I don't have perms to warn this person");
+    var warnUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
+    var reason = args.slice(2).join(" ");
+    if (!warnUser) return message.reply("Couldn't find that user!");
+    if (warnUser.hasPermission("MANAGE_MESSAGE")) return message.reply("You can\'t warn this user!");
+    if (!warns[warnUser.id]) warns[warnUser.id] ={
+        warns: 0
+    };
+    warns[warnUser.id].warns++;
+    fs.writeFile("./warnings.json", JSON.stringify(warns), (err) =>{
+        if (err) console.log(err);
+    });
+    var embed = new Discord.MessageEmbed()
+        .setColor("0x17CFEC")
+        .setFooter(message.member.displayName, message.author.displayAvatarURL)
+        .setTitle('**Warned** ${warnUser} (${warnUser.id})')
+        .setDescription('**Reason:** ${reason}')
+        .addField("Amount of warns", warns[warnUser.id].warns);
+    var channel = message.member.guild.channels.chache.get("740647204456497193");
+    if(!channel) return;
+    channel.send(embed);
+}
 module.exports = {
     name: 'warn',
-    description: "warn a user with this command!",
-    execute(message, args){
-        if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply("You don\'t have permissions to use this command!");
-        if (!args[1]) return message.reply('You did not specify a user.');
-        if (!args[2]) return message.reply('You did not give a reasoning.')
-        if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.reply("I don't have perms to warn this person");
-        var warnUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
-        var reason = args.slice(2).join(" ");
-        if (!warnUser) return message.reply("Couldn't find that user!");
-        if (warnUser.hasPermission("MANAGE_MESSAGE")) return message.reply("You can\'t warn this user!");
-        if (!warns[warnUser.id]) warns[warnUser.id] ={
-            warns: 0
-        };
-        warns[warnUser.id].warns++;
-        fs.writeFile("./warnings.json", JSON.stringify(warns), (err) =>{
-            if (err) console.log(err);
-        });
-        var embed = new Discord.MessageEmbed()
-            .setColor("0x17CFEC")
-            .setFooter(message.member.displayName, message.author.displayAvatarURL)
-            .setTitle('**Warned** ${warnUser} (${warnUser.id})')
-            .setDescription('**Reason:** ${reason}')
-            .addField("Amount of warns", warns[warnUser.id].warns);
-        var channel = message.member.guild.channels.chache.get("740647204456497193");
-        if(!channel) return;
-        channel.send(embed);
     }
-
-}
