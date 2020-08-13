597206a2f2ffae1315d970576bb31b3bdc66d816
W0ulf <wout.depit@gmail.com>
2020-08-13 16:38:20 +0200

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'warn',
-    execute(message, args){
+    execute(async, message, args){
     
         if(!message.member.hasPermission("ADMINISTRATOR")) {
             return message.channel.send("You don't have permissions to use this command!")
