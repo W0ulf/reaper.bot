37e9711091e81d69c551ed974cbc01522ce1a9a0
W0ulf <wout.depit@gmail.com>
2020-10-16 19:46:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ const PREFIX = '-';
 const bot = new Discord.Client();
 const fs = require("fs");
 
-bot.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
-
-    bot.commands.set(command.name, command);
-}
-
 var version = '1.0.4'
 
 const usedCommandRecently = new Set();
