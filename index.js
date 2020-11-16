c8b2e43dc22b4fa02b539103d1a9a92a08ad67c6
W0ulf <67956717+W0ulf@users.noreply.github.com>
2020-11-16 20:12:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ bot.on('ready', () =>{
                
                     if(!role) return message.reply("Couldn't find the red role.")
      
-                    person.roles.add('777971361607450645');
+                    person.roles.add(role.id);
      
                     message.channel.send(`@${person.user.tag} has now been giving the red role!`)
                 } else {
